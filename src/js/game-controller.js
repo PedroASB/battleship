1e1232293d4e5dba19326d3e406489c0c6b5ed83
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-11-19 16:17:49 -0300

    feat(game-controller): add a game controller module with main routines
    - This module manages a battleship game execution. This is an initial
      implementation with some temporary features
    - Implemented a handleAttack() method as the routine to be executed
      when an square's event listener is triggered. This routine is also
      responsible for check winning conditions
    - Implemented a startGame() method to perform initial setups and
      start the game

---
+++ src/js/game-controller.js
@@ -0,0 +1,73 @@
+import * as domManager from './dom-manager.js';
+
+function delay(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+export default class GameController {
+  #attacker;
+  #receiver;
+
+  constructor(playerOne, playerTwo) {
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+  }
+
+  #swapAttacker() {
+    [this.#attacker, this.#receiver] = [this.#receiver, this.#attacker];
+    domManager.unsetTarget(this.#attacker.getId());
+    domManager.setTarget(this.#receiver.getId());
+    domManager.displayCurrentTurnMessage(this.#attacker.name);
+  }
+
+  #checkWinning() {
+    return this.playerOne.areAllShipsSunk() || this.playerTwo.areAllShipsSunk();
+  }
+
+  #handleWinning() {
+    domManager.unsetTarget(this.#receiver.getId());
+    domManager.showFleet(this.#attacker.getId());
+    domManager.showFleet(this.#receiver.getId());
+    domManager.showWinningMessage(this.#attacker.name); // temporary functionality
+  }
+
+  async #handleAttack(player, coordinates) {
+    if (player !== this.#receiver || player.isAttackedAt(coordinates)) {
+      return;
+    }
+
+    domManager.displayFeedbackMessage('Performing the attack...');
+    domManager.clearCurrentTurnMessage();
+    await delay(0.25);
+
+    player.receiveAttack(coordinates);
+    domManager.receiveAttack(player.getId(), coordinates);
+    domManager.displayFeedbackMessage(
+      `${this.#attacker.name} attacked ${this.#receiver.name} at [${coordinates.x}, ${coordinates.y}]`,
+    );
+
+    if (this.#checkWinning()) {
+      this.#handleWinning();
+      return;
+    }
+
+    this.#swapAttacker();
+  }
+
+  #initializePlayerBox(player) {
+    domManager.initializePlayerBox(player, this.#handleAttack.bind(this, player));
+  }
+
+  startGame() {
+    domManager.clearPlayersSection();
+    this.#initializePlayerBox(this.playerOne);
+    this.#initializePlayerBox(this.playerTwo);
+
+    this.#attacker = this.playerOne;
+    this.#receiver = this.playerTwo;
+
+    domManager.setTarget(this.#receiver.getId());
+    domManager.displayFeedbackMessage(`The battleship has started!`);
+    domManager.displayCurrentTurnMessage(this.#attacker.name);
+  }
+}
